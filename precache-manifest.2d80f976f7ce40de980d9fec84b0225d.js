8422a9aa1e54a72858f77219e32d73733384e87b
soumyadip-me <57860501+soumyadip-me@users.noreply.github.com>
2019-11-17 16:47:57 +0530

    add files

---
+++ precache-manifest.2d80f976f7ce40de980d9fec84b0225d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "679ff19f51a99862bfec4424aa754a00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2775190c170133dd40e",
+    "url": "/static/css/main.85e69327.chunk.css"
+  },
+  {
+    "revision": "e0f08819bcaace8d17b4",
+    "url": "/static/js/2.e7aa16ca.chunk.js"
+  },
+  {
+    "revision": "f2775190c170133dd40e",
+    "url": "/static/js/main.35e46904.chunk.js"
+  },
+  {
+    "revision": "475989f7ab49b3903971",
+    "url": "/static/js/runtime-main.26768bba.js"
+  },
+  {
+    "revision": "139ea4cfb4fd08c14cfada3afa78bdca",
+    "url": "/static/media/android.139ea4cf.svg"
+  },
+  {
+    "revision": "283b259502f439fd6bae85fb80e33929",
+    "url": "/static/media/facebook.283b2595.svg"
+  },
+  {
+    "revision": "5a84a9749f010452bbc15579eff0f947",
+    "url": "/static/media/gmail.5a84a974.svg"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/static/media/left-arrow.6c9efe0c.svg"
+  },
+  {
+    "revision": "00f23d41fb819a5470ecd841ae625fc1",
+    "url": "/static/media/linkedin.00f23d41.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/static/media/right-arrow.be237328.svg"
+  },
+  {
+    "revision": "e15a8effdae039d64a0d88ef89c97acc",
+    "url": "/static/media/scExpanded.e15a8eff.svg"
+  },
+  {
+    "revision": "fccd450e84714dad09e79ecdffcbb211",
+    "url": "/static/media/scSmall.fccd450e.svg"
+  },
+  {
+    "revision": "169203880da8e18e58ee8f36120c68b1",
+    "url": "/static/media/web.16920388.svg"
+  },
+  {
+    "revision": "3c06d8aa4fe1799d0eb2cbaba94046f1",
+    "url": "/static/media/whatsapp.3c06d8aa.svg"
+  }
+]);
